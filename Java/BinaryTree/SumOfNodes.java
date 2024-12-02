34190472514bdaa0c95537b5dd51a595464ae8ec
Niraj <nirajprasad4y18@gmail.com>
2024-12-02 14:19:29 +0530

    Sum of all Nodes in tree

---
+++ Java/BinaryTree/SumOfNodes.java
@@ -0,0 +1,44 @@
+package BinaryTree;
+
+// Class to represent a Node in the tree
+class Node {
+    int data;
+    Node left, right;
+
+    // Constructor to create a new Node
+    public Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+public class SumOfNodes {
+
+    public static int sumOfNodes(Node root) {
+        // Base case: if the node is null, return 0
+        if (root == null) {
+            return 0;
+        }
+
+        // Recursively sum nodes in the left and right subtrees
+        int leftSum = sumOfNodes(root.left);
+        int rightSum = sumOfNodes(root.right);
+
+        return leftSum + rightSum + root.data;
+    }
+
+    public static void main(String[] args) {
+        // Create a binary tree
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        // Sum the nodes in the tree
+        int nodeSum = sumOfNodes(root);
+
+        // Print the result
+        System.out.println("Total number of nodes in the tree: " + nodeSum);
+    }
+}
