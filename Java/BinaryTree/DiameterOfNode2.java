9a3c94fb6089e594eb031775d9768965281cf4d2
Niraj <nirajprasad4y18@gmail.com>
2024-12-03 08:24:08 +0530

    Approach 2 = 0(n)

---
+++ Java/BinaryTree/DiameterOfNode2.java
@@ -0,0 +1,59 @@
+package BinaryTree;
+//Approach 2 : 0(n)
+class Node
+{
+    int data;
+    Node left , right;
+
+    public Node(int item)
+    {
+        data = item ;
+        left = right = null;
+    }
+}
+public class DiameterOfNode2
+{
+    static class TreeInfo
+    {
+        int ht;
+        int diam;
+
+        TreeInfo(int ht, int diam)
+        {
+            this.ht = ht;
+            this.diam = diam;
+        }
+    }
+
+    public static TreeInfo diameter2(Node root)
+    {
+        if(root == null)
+        {
+            return new TreeInfo(0, 0);
+        }
+        TreeInfo left = diameter2(root.left);
+        TreeInfo right = diameter2(root.right);
+
+        int myHeight = Math.max(left.ht, right.ht) + 1;
+
+        int diam1 = left.diam;
+        int diam2 = right.diam;
+        int diam3 = left.ht + right.ht + 1;
+
+        int myDiam = Math.max(Math.max(diam1, diam2), diam3);
+
+        TreeInfo myInfo = new TreeInfo(myHeight, myDiam);
+        return myInfo;
+    }
+    public static void main(String[] args)
+    {
+        // Create a binary tree
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        System.out.println(diameter2(root).diam);
+    }
+}
