8b9276ca2aa00eb130e5f706584bd8057175674f
Niraj <nirajprasad4y18@gmail.com>
2024-12-08 16:53:13 +0530

    Reverse Word

--- Java/reverse_words.java
+++ Java/reverse_words.java
@@ -1,15 +1,15 @@
 import java.util.*;
-public class reverse_words 
+public class reverse_words
 {
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
         System.out.print("Enter Word: ");
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         String rev = "";
 
-        for(int i = str.length() - 1; i >= 0; i--)
-        rev += str.charAt(i);
+        for(int i = 0 ; i < str.length(); i++)
+        rev = str.charAt(i) + rev;
         System.out.println("Reverse of given  word: " + rev);
 
         sc.close();
