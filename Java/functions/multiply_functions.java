ec286b8084f4c16f435398530cb3eb45a50afc9c
Niraj <nirajprasad4y18@gmail.com>
2024-12-09 18:14:11 +0530

    Multiply

--- Java/functions/multiply_functions.java
+++ Java/functions/multiply_functions.java
@@ -1,24 +1,23 @@
 package functions;
- 
+
 import java.util.Scanner;
-public class multiply_functions 
+public class multiply_functions
 {
    //Multiply 2 numbers
-   public static int multiply(int a, int b) 
-   {
+    public static int multiply(int a, int b)
+    {
        return a * b;
-   }
-   public static void main(String args[]) 
-   {
-       Scanner sc = new Scanner(System.in);
-       System.out.print("a = ");
-       int a = sc.nextInt();
-       System.out.print("b = ");
-       int b = sc.nextInt();
-
-       int result = multiply(a, b);
-       System.out.println("Multiply of a and b : " + result);
-       sc.close();
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("a = ");
+        int a = sc.nextInt();
+        System.out.print("b = ");
+        int b = sc.nextInt();
+        
+        int result = multiply(a, b);
+        System.out.println("Multiply of a and b : " + result);
+        sc.close();
     }
 }
-
