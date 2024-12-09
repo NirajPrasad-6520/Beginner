136b98cc8a682386b3dbd6091bfd7e57c28f3595
Niraj <nirajprasad4y18@gmail.com>
2024-12-09 18:18:36 +0530

    Average

--- Java/functions/Exercise/average.java
+++ Java/functions/Exercise/average.java
@@ -1,14 +1,14 @@
 package functions.Exercise;
 // Enter 3 numbers from the user & make a function to print their average.
 import java.util.*;
-public class average 
+public class average
 {
     public static void printAverage(int a, int b, int c)
     {
         int avg = (a + b + c) / 3;
         System.out.println("Average of a, b, c : " + avg);
     }
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
         System.out.print("a : ");
@@ -21,8 +21,6 @@ public class average
         int c = sc.nextInt();
         
         printAverage(a, b, c);
-        sc.close();        
+        sc.close();
     }
-
-       
 }
