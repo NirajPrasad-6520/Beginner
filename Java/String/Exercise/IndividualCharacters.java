387b69efb2fe35f4a1d29f6cce7c299f21fd64b7
Niraj <nirajprasad4y18@gmail.com>
2024-12-09 19:54:15 +0530

    Individual Characters

--- Java/String/Exercise/IndividualCharacters.java
+++ Java/String/Exercise/IndividualCharacters.java
@@ -1,14 +1,20 @@
 package String.Exercise;
-public class IndividualCharacters 
+
+public class IndividualCharacters
 {
-    public static void main(String[] args) 
-    {  
-        String string = "characters ";  
-        System.out.println("Individual characters from given string: ");  
-  
-        for(int i = 0; i < string.length(); i++)
-        {  
-            System.out.print(string.charAt(i) + "  ");  
-        }  
-    }  
+    public static void main(String[] args)
+    {
+        // Input string to be processed
+        String string = "characters ";
+
+        // Output heading to explain what is being printed
+        System.out.println("Individual characters from given string: ");
+
+        // Loop through each character in the string
+        for (int i = 0; i < string.length(); i++)
+        {
+            // Print each character followed by a space
+            System.out.print(string.charAt(i) + "  ");
+        }
+    }
 }
