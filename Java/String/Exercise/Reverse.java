8e08ae53383d6cd5aa9117998886d71c5307c398
Niraj <nirajprasad4y18@gmail.com>
2024-12-09 19:55:02 +0530

    Reverse Sentence

--- Java/String/Exercise/Reverse.java
+++ Java/String/Exercise/Reverse.java
@@ -1,15 +1,22 @@
 package String.Exercise;
-public class Reverse 
+
+public class Reverse
 {
-    public static void main(String[] args) {    
-        String str = "Hello World";    
-        String rev = "";    
+    public static void main(String[] args)
+    {
+        String str = "Hello World";
+
+        // Initialize an empty string to store the reversed string
+        String rev = "";
             
-        for(int i = str.length()-1; i >= 0; i--)
-        {    
-            rev = rev + str.charAt(i);    
-        }    
-        System.out.println("Original string: " + str);    
-        System.out.println("Reverse of given string: " + rev);    
-    }    
+        // Loop through the string from the last character to the first
+        for (int i = str.length() - 1; i >= 0; i--)
+        {
+            // Append each character from the end to the 'rev' string
+            rev = rev + str.charAt(i);
+        }
+        System.out.println("Original string: " + str);
+
+        System.out.println("Reverse of given string: " + rev);
+    }
 }
