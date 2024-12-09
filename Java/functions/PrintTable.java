85e28bc51bdeff6dbf9d012bd063d532f8168a24
Niraj <nirajprasad4y18@gmail.com>
2024-12-09 18:07:17 +0530

    Print Table

--- Java/functions/PrintTable.java
+++ Java/functions/PrintTable.java
@@ -1,13 +1,13 @@
 package functions;
 import java.util.*;
-public class PrintTable 
+public class PrintTable
 {
     public static void printtable(int n)
     {
         for(int i = 1; i <= 10; i++)
         System.out.println(n + " * " + i + " = " + n*i);
     }
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number for any table: ");
