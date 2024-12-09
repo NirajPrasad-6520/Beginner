49364f578c086a89ed8b03fd44e086b7b7bbbf7e
Niraj <nirajprasad4y18@gmail.com>
2024-12-09 19:57:42 +0530

    without using the reverse function

--- Java/String/Exercise/WithoutReverseFunction1.java
+++ Java/String/Exercise/WithoutReverseFunction1.java
@@ -1,23 +1,30 @@
 package String.Exercise;
-//How to reserve a string in Java without using reverse function
+// Java program to reverse a string without using the reverse function
 import java.util.*;
-public class WithoutReverseFunction1 
+public class WithoutReverseFunction1
 {
     public static void main(String[] arg)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a string : ");
-        String  s = sc.nextLine();
-        System.out.println("Reverse String  is : " + WithoutReverseFunction1.reverse(s));
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+        
+        // Call the static reverse() method and print the reversed string
+        System.out.println("Reverse String is: " + WithoutReverseFunction1.reverse(s));
+
         sc.close();
     }
-        static String reverse(String str)
+    static String reverse(String str)
+    {
+        // Initialize an empty string to store the reversed result
+        String rev = "";
+
+        // Loop to iterate through the string from the last character to the first character
+        for (int i = str.length(); i > 0; --i)
         {
-            String rev =  "";  
-            for(int i = str.length();i > 0; --i)  
-            {  
-                rev = rev + (str.charAt(i - 1));   
-            }  
-            return rev;  
-        }      
+            // Append each character to the result string in reverse order
+            rev = rev + str.charAt(i - 1);
+        }
+        return rev;
+    }
 }
