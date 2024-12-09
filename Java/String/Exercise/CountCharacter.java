8612cec559645c564c3eace7cb4910ee29286040
Niraj <nirajprasad4y18@gmail.com>
2024-12-09 19:52:51 +0530

    Count Character'

--- Java/String/Exercise/CountCharacter.java
+++ Java/String/Exercise/CountCharacter.java
@@ -1,28 +1,33 @@
 package String.Exercise;
-public class CountCharacter 
-{
-    public static void main(String[] args) 
-    {
-        String s = "An object that represents a number of character values";
-        int count = 0;
 
-        for(int i = 0; i < s.length(); i++ )
+public class CountCharacter
+{
+    public static void main(String[] args)
+    {
+        // Input string
+        String s = "An object that represents a number of character values";
+        int count = 0; // Counter to count non-space characters
+
+        // Loop through each character in the string
+        for (int i = 0; i < s.length(); i++)
         {
-            if (s.charAt(i) != ' ') 
+            // If the current character is not a space, increment the count
+            if (s.charAt(i) != ' ')
             {
-                count++;                
+                count++;
             }
         }
-        s = s.replaceAll("\\s+", "");    
-            
-        System.out.println("String after remove spaces : " + s);  
-        System.out.println(count);   
-        
+
+        // Removes all spaces from the string using regex
+        s = s.replaceAll("\\s+", "");
+        System.out.println("String after removing spaces : " + s);  // Outputs the string without spaces
+        System.out.println("Number of non-space characters: " + count); // Outputs the count of non-space characters
+
+        // Another input string
         String ss = "An object that represents a number of character values";
                     
-        //Replace space with specific character ch    
-        ss = ss.replace(' ', '-');    
-            
-        System.out.println("String after replacing spaces with given character: " + ss);  
-    }    
+        // Replace all spaces with a specific character ('-')
+        ss = ss.replace(' ', '-');
+        System.out.println("String after replacing spaces with given character: " + ss); // Outputs the string with spaces replaced by '-'
+    }
 }
