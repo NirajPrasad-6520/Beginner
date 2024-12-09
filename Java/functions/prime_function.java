36deb3c94d455ba44267517641581d73fb5cf548
Niraj <nirajprasad4y18@gmail.com>
2024-12-09 18:12:49 +0530

    Check Prime

--- Java/functions/prime_function.java
+++ Java/functions/prime_function.java
@@ -1,8 +1,7 @@
 package functions;
 
 import java.util.Scanner;
-
-public class prime_function 
+public class prime_function
 {
     public static void printPrime(int n)
     {
@@ -16,7 +15,7 @@ public class prime_function
         else
             System.out.println("Not Prime Number.");
     }
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number to check Prime or Not :");
