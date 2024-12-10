2696eb8d7928e73dbd49d404d99971aeac9d47f8
Niraj <nirajprasad4y18@gmail.com>
2024-12-10 18:38:34 +0530

    Method Array

--- Java/Array/SingleDimensionalArray/MethodArray.java
+++ Java/Array/SingleDimensionalArray/MethodArray.java
@@ -1,21 +1,29 @@
 package Array.SingleDimensionalArray;
 
-public class MethodArray 
+public class MethodArray
 {
-    //Java Program to demonstrate the way of passing an array to method.  
+    // Java Program to demonstrate the way of passing an array to a method
 
-    //creating a method which receives an array as a parameter  
+    // Creating a method which receives an array as a parameter
     static void min(int arr[])
-    {  
-       int min = arr[0];  
-          for(int i = 1; i < arr.length; i++)  
-            if(min > arr[i])  
-              min = arr[i];
-            System.out.println(min);  
-    }  
+    {
+        // Initialize min with the first element of the array
+        int min = arr[0];
+        // Loop through the array to find the smallest element
+        for(int i = 1; i < arr.length; i++)
+        {
+            // Update min if the current element is smaller
+            if(min > arr[i])
+                min = arr[i];
+        }
+        // Print the smallest element found
+        System.out.println(min);
+    }
     public static void main(String args[])
-    {  
-        int a[] = {33, 3, 4, 5, 1, 0};//declaring and initializing an array  
-        min(a);//passing array to method  
+    {
+        // Declare and initialize an array
+        int a[] = {33, 3, 4, 5, 1, 0};
+        // Passing the array to the min() method
+        min(a);
     }
 }
