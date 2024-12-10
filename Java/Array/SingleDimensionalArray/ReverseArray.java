9f91a35e313a8f8066eb4c88d80b5934b0ef4cb7
Niraj <nirajprasad4y18@gmail.com>
2024-12-10 18:40:22 +0530

    Reverse Array

--- Java/Array/SingleDimensionalArray/ReverseArray.java
+++ Java/Array/SingleDimensionalArray/ReverseArray.java
@@ -1,8 +1,8 @@
 package Array.SingleDimensionalArray;
 
-public class ReverseArray 
+public class ReverseArray
 {
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
         int arr[] = new int[]{1,2,3,4,5};
         System.out.println("Original Array: ");
@@ -13,7 +13,6 @@ public class ReverseArray
         //Reverse
         System.out.println("Reverse Array :");
         for(int i = arr.length - 1; i >= 0; i--)
-        System.out.print(arr[i] + " ");        
+        System.out.print(arr[i] + " ");
     }
-    
 }
