bc7d01725f13f5900269b5dc6aed1c4b4956003b
Niraj <nirajprasad4y18@gmail.com>
2024-12-10 13:58:19 +0530

    Print x^n (stack height = n)

--- Java/Recursion/Recursion6.java
+++ Java/Recursion/Recursion6.java
@@ -1,22 +1,27 @@
 package Recursion;
 //Print x^n (stack height = n).
-public class Recursion6 
+
+public class Recursion6
 {
-        public static int power(int x, int n) 
+    // Recursive method to calculate x^n
+    public static int power(int x, int n)
+    {
+        // Base case: any number raised to the power of 0 is 1
+        if (n == 0)
         {
-            if (n == 0) 
-            {
-                return 1;
-            } 
-            else 
-            {
-                return x * power(x, n - 1);
-            }
+            return 1;
         }
-        public static void main(String[] args) 
+        else
         {
-            int x = 2, n = 5;
-            int result = power(x, n);
-            System.out.println(x + "^" + n + " = " + result); 
+            // Recursive step: x * (x^(n-1))
+            return x * power(x, n - 1);
         }
+    }
+    public static void main(String[] args)
+    {
+        int x = 2, n = 5; // Variables for base and exponent
+        int result = power(x, n); // Compute x^n using recursion
+        
+        System.out.println(x + "^" + n + " = " + result);
+    }
 }
