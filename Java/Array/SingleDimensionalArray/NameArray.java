dca934ba29b3de7e8c24e6fa1e5460502056febf
Niraj <nirajprasad4y18@gmail.com>
2024-12-10 18:39:28 +0530

    Name Array

--- Java/Array/SingleDimensionalArray/NameArray.java
+++ Java/Array/SingleDimensionalArray/NameArray.java
@@ -1,24 +1,25 @@
 package Array.SingleDimensionalArray;
-// Take an array of names as input from the user and print them on the screen.
+// Program to take an array of names as input from the user and print them on the screen
 import java.util.*;
-public class NameArray 
+public class NameArray
 {
-   public static void main(String args[]) 
+   public static void main(String args[])
    {
       Scanner sc = new Scanner(System.in);
-      System.out.print("Enter a number that how many name's you want to add: ");
-      int size = sc.nextInt();//here size means how many name's you want to add so give number for size. for example if size = 3 then 3 name add.
+      System.out.print("Enter a number that represents how many names you want to add: ");
+      int size = sc.nextInt(); // Read the number of names to be added
+   
       String names[] = new String[size];
-      System.out.println("Enter name :");
       
-      //input
-      for(int i = 0; i < size; i++) 
-          names[i] = sc.next();
+      System.out.println("Enter names:");
 
-      //output
-       for(int i = 0; i < names.length; i++) 
-           System.out.println("Name " + (i + 1) +" is : " + names[i]);
+      // Input: Take names as input from the user
+      for(int i = 0; i < size; i++)
+          names[i] = sc.next(); // Read each name and store it in the array
+
+      // Output: Display the names entered by the user
+      for(int i = 0; i < names.length; i++)
+         System.out.println("Name " + (i + 1) + " is: " + names[i]);
       sc.close();
    }
-
 }
