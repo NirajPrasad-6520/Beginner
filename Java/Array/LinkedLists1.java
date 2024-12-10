b186329ae3bb8c0e0aca61e7657432822cfd27f4
Niraj <nirajprasad4y18@gmail.com>
2024-12-10 19:32:31 +0530

    Adding elements to the LinkedList

--- Java/Array/LinkedLists1.java
+++ Java/Array/LinkedLists1.java
@@ -1,39 +1,42 @@
 package Array;
+
 import java.util.LinkedList;
-public class LinkedLists1 
+public class LinkedLists1
 {
-        public static void main(String[] args) 
+    public static void main(String[] args)
     {
-        // Create a new linked list
         LinkedList<String> l = new LinkedList<String>();
 
-        //adding elements to the LinkedList using add() method
+        // Adding elements to the LinkedList using add() method
         l.add("One");
         l.add("Two");
         l.add("Three");
         l.add("Four");
         l.add("Five");
-        l.addLast("Ten");// Add an element to the end of the linked list
-        l.addFirst("Zero");//Add an element to the beginning of the linked list
-        l.add(1,"A");
-        
-        System.out.println("Initial LinkedList : " + l +"\nThe size of the linked list is: "+ l.size());
+        l.addLast("Ten");
+        l.addFirst("Zero");
+        l.add(1, "A");
 
-        //Linked list to To Array by using  toArray();
-        Object[] a = l.toArray();
+        // Display the initial LinkedList and its size
+        System.out.println("Initial LinkedList : " + l + "\nThe size of the linked list is: " + l.size());
+
+        // Convert LinkedList to an Array using toArray() method
+        Object[] a = l.toArray(); // Convert the LinkedList to an array
         System.out.print("After converted LinkedList to Array: ");
         for(Object element : a)
-        System.out.print(element+" ");
-
+        {
+            // Iterate through the array and print its elements
+            System.out.print(element + " ");
+        }
         System.out.println();
 
-        //remove element
-        l.set(1, "set");//After adding the elements, if we wish to change the element, it can be done using the set() method.
-        l.remove(3);//by index number
-        l.remove("Four");//by value
-        l.removeFirst();
-        l.removeLast();
-        
-        System.out.println("Update LinkedList : " + l);
+        // Modifying and removing elements in the LinkedList
+        l.set(1, "set"); // Updates the element at index 1 to "set"
+        l.remove(3);  // Removes the element at index 3
+        l.remove("Four");// Removes the first occurrence of the element "Four"
+        l.removeFirst();  // Removes the first element of the LinkedList
+        l.removeLast();  // Removes the last element of the LinkedList
+
+        System.out.println("Updated LinkedList : " + l);
     }
 }
