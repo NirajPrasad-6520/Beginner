1916406a280f8bc2cec7f6a86a01bec3852a9105
Niraj <nirajprasad4y18@gmail.com>
2024-12-10 19:33:45 +0530

    Reverse the linked list iteratively

--- Java/Array/ReverseLinkedLists.java
+++ Java/Array/ReverseLinkedLists.java
@@ -1,79 +1,91 @@
 package Array;
+class Node
+{
+    int data;    // Data stored in the node
+    Node next;   // Reference to the next node in the list
 
-class Node {
-    int data;
-    Node next;
-
-    // Constructor to create a new node
-    Node(int data) {
+    // Constructor to create a new node with given data
+    Node(int data)
+    {
         this.data = data;
-        this.next = null;
+        this.next = null; // Initially, the next pointer is null
     }
 }
-
-class LL {
-    Node head;
+// Class to represent the LinkedList
+class LL
+{
+    Node head; // Head of the LinkedList
 
     // Method to add a node at the end of the list
-    public void addLast(int data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
+    public void addLast(int data)
+    {
+        Node newNode = new Node(data); // Create a new node with the given data
+        if (head == null) // If the list is empty
+        {
+            head = newNode;// Set the new node as the head
             return;
         }
-        Node temp = head;
-        while (temp.next != null) {
+        Node temp = head;// Start from the head
+        while (temp.next != null) // Traverse to the last node
+        {
             temp = temp.next;
         }
-        temp.next = newNode;
+        temp.next = newNode;  // Link the last node to the new node
     }
-
     // Method to print the linked list
-    public void printList() {
-        if (head == null) {
+    public void printList()
+    {
+        if (head == null)// If the list is empty
+        {
             System.out.println("List is empty.");
             return;
         }
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + " -> ");
-            temp = temp.next;
+        Node temp = head; // Start from the head
+        while (temp != null)// Traverse until the end of the list
+        {
+            System.out.print(temp.data + " -> "); // Print the data
+            temp = temp.next;// Move to the next node
         }
-        System.out.println("null");
+        System.out.println("null");// Indicate the end of the list
     }
 
     // Method to reverse the linked list iteratively
-    public void reverseIterate() {
-        if (head == null || head.next == null) {
+    public void reverseIterate()
+    {
+        if (head == null || head.next == null)// If the list has 0 or 1 node, no need to reverse
+        {
             return;
         }
 
-        Node prevNode = null;
-        Node currNode = head;
-        while (currNode != null) {
-            Node nextNode = currNode.next; // Save the next node
-            currNode.next = prevNode; // Reverse the link
-            prevNode = currNode; // Move prevNode one step ahead
-            currNode = nextNode; // Move currNode one step ahead
+        Node prevNode = null;     // Previous node starts as null
+        Node currNode = head;    // Current node starts as the head
+        while (currNode != null)// Traverse the list
+        {
+            Node nextNode = currNode.next;// Save the next node
+            currNode.next = prevNode;    // Reverse the current node's link
+            prevNode = currNode;        // Move prevNode one step ahead
+            currNode = nextNode;       // Move currNode one step ahead
         }
-        head = prevNode; // Update the head to the new first node
+        head = prevNode;// Update the head to the new first node
     }
 }
-
-public class ReverseLinkedLists {
-    public static void main(String[] args) {
-        LL list = new LL();
-        list.addLast(1);
+// Main class to demonstrate the LinkedList operations
+public class ReverseLinkedLists
+{
+    public static void main(String[] args)
+    {
+        LL list = new LL(); // Create a new LinkedList
+        list.addLast(1);    // Add nodes to the list
         list.addLast(2);
         list.addLast(3);
         list.addLast(4);
 
         System.out.println("Original List:");
-        list.printList();
+        list.printList();   // Print the original list
 
-        list.reverseIterate();
+        list.reverseIterate(); // Reverse the list iteratively
 
         System.out.println("Reversed List:");
-        list.printList();
+        list.printList();   // Print the reversed list
     }
 }
