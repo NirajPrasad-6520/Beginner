7c51cb6cebe4a02c51c1504da97141fdb8c364bc
Niraj <nirajprasad4y18@gmail.com>
2024-12-15 18:03:56 +0530

    first commit

--- Java/Practice/practice1.java
+++ Java/Practice/practice1.java
@@ -1,29 +1,20 @@
 package Practice;
-
-import java.util.Scanner;
-
+import java.util.Arrays;
 public class practice1
 {
+    public static void pnumber(int n)
+    {
+        if(n == 6)
+        {
+            return;
+        }
+        System.out.println(n);
+
+        pnumber(n + 1);
+    }
     public static void main(String[] args)
     {
-        System.out.println("my first practice program.");
-        
-        int n = 10,i,j,count,total = 0;
-        for(i = 1; i <= n; i++)
-        {
-            count = 0;
-            for(j = 1; j <= i; j++)
-            {
-                if(i % j == 0)
-                count++;
-            }
-            if(count == 2)
-            {
-                System.out.print(i + " ");
-                total = total + 1;
-            }
-        }
-        System.out.println();
-        System.out.println("Total " + total);
-    }
+       int n = 0;
+       pnumber(n);
+}
 }
