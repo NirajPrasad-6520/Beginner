caac76c93c5b2c2032e5e8070c57c0ae26c8ffc6
Niraj <nirajprasad4y18@gmail.com>
2024-12-16 10:42:37 +0530

    Print Range between Two Numbers

--- Java/BinaryTree/BST/BSTRangePrinter.java
+++ Java/BinaryTree/BST/BSTRangePrinter.java
@@ -1,59 +1,61 @@
 package BinaryTree.BST;
 
-    class TreeNode
+class TreeNode
+{
+    int val; // Value of the node
+    TreeNode left, right;  // Left and right child nodes
+
+    // Constructor to create a new node with a given value
+    TreeNode(int val)
     {
-        int val;
-        TreeNode left, right;
-    
-        TreeNode(int val) {
-            this.val = val;
-            left = right = null;
+        this.val = val;
+        left = right = null;  // Initialize left and right children as null
+    }
+}
+
+public class BSTRangePrinter
+{
+    // Method to print all the nodes in the BST that are in the range [x, y]
+    public void printRange(TreeNode root, int x, int y)
+    {
+        // Base case: If the current node is null, return
+        if (root == null) {
+            return;
+        }
+
+        // If the current node's value is greater than the lower bound, recursively traverse the left subtree
+        if (root.val > x)
+        {
+            printRange(root.left, x, y);
+        }
+
+        // If the current node's value is in the range [x, y], print it
+        if (root.val >= x && root.val <= y)
+        {
+            System.out.print(root.val + " ");
+        }
+
+        // If the current node's value is less than the upper bound, recursively traverse the right subtree
+        if (root.val < y)
+        {
+            printRange(root.right, x, y);
         }
     }
-    
-    public class BSTRangePrinter
+    public static void main(String[] args)
     {
-        // Method to print nodes in the given range
-        public void printRange(TreeNode root, int x, int y)
-        {
-            if (root == null)
-            {
-                return;
-            }
-    
-            // Traverse the left subtree if the current node's value is greater than the lower bound
-            if (root.val > x)
-            {
-                printRange(root.left, x, y);
-            }
-    
-            // Print the current node if it lies within the range
-            if (root.val >= x && root.val <= y)
-            {
-                System.out.print(root.val + " ");
-            }
-    
-            // Traverse the right subtree if the current node's value is less than the upper bound
-            if (root.val < y)
-            {
-                printRange(root.right, x, y);
-            }
-        }
-        public static void main(String[] args)
-        {
-            // Creating a sample BST
-            TreeNode root = new TreeNode(10);
-            root.left = new TreeNode(5);
-            root.right = new TreeNode(20);
-            root.left.left = new TreeNode(3);
-            root.left.right = new TreeNode(7);
-            root.right.left = new TreeNode(15);
-            root.right.right = new TreeNode(25);
-    
-            BSTRangePrinter printer = new BSTRangePrinter();
-    
-            // Print values in range [7, 20]
-            System.out.println("Nodes in range [7, 20]:");
-            printer.printRange(root, 7, 20);
-        }
+        // Create a sample BST
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(20);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(7);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(25);
+
+        // Create an instance of BSTRangePrinter to use the printRange method
+        BSTRangePrinter printer = new BSTRangePrinter();
+
+        System.out.println("Nodes in range [7, 20]:");
+        printer.printRange(root, 7, 20);
     }
+}
