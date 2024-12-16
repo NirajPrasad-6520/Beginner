a8ab3f5adcdaea6fa1a317da82dbbc12c53ff30c
Niraj <nirajprasad4y18@gmail.com>
2024-12-16 10:35:57 +0530

    Height of a Tree

--- Java/BinaryTree/HeightOfATree.java
+++ Java/BinaryTree/HeightOfATree.java
@@ -5,7 +5,7 @@ class Node
     int data;
     Node left, right;
 
-    // Constructor to create a new Node
+    // Constructor to initialize node data and children as null
     public Node(int item)
     {
         data = item;
@@ -17,16 +17,22 @@ public class HeightOfATree
 {
     public static int height(Node root)
     {
-        if(root == null) return 0;
+        if (root == null)
+        {
+            // Base case: If tree is empty, height is 0
+            return 0;
+        }
 
+        // Recursive calls to calculate the height of left and right subtrees
         int leftHeight = height(root.left);
         int rightHeight = height(root.right);
 
-        int myHeight = Math.max(leftHeight , rightHeight) + 1 ;
-
-        return myHeight;
+        // Height of the current node is the maximum of left and right subtree heights, plus 1
+        return Math.max(leftHeight, rightHeight) + 1;
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args)
+    {
         // Create a binary tree
         Node root = new Node(1);
         root.left = new Node(2);
@@ -34,9 +40,9 @@ public class HeightOfATree
         root.left.left = new Node(4);
         root.left.right = new Node(5);
 
-        int nodeHeight = height(root);
+        // Calculate the height of the tree
+        int treeHeight = height(root);
 
-        // Print the result
-        System.out.println("Height of a tree: " + nodeHeight);
+        System.out.println("Height of the tree: " + treeHeight);
     }
 }
