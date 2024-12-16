b0c7a9799fc9d07704ece0311ecb593208367d9f
Niraj <nirajprasad4y18@gmail.com>
2024-12-16 10:36:23 +0530

    Left Skewed Binary Tree

--- Java/BinaryTree/LeftSkewed.java
+++ Java/BinaryTree/LeftSkewed.java
@@ -2,39 +2,38 @@ package BinaryTree;
 
 public class LeftSkewed
 {
-// A Tree node
-static class Node
-{
-	int key;
-	Node left, right;
-}
-// Utility function to create a new node
-static Node newNode(int key)
-{
-	Node temp = new Node();
-	temp.key = key;
-	temp.left = temp.right = null;
-
-	return (temp);
-}
-public void display(Node root)
-{
-    if(root == null)
+    static class Node
     {
-        return;
+        int key; // Value of the node
+        Node left, right; // Left and right child references
     }
-    System.out.print(root.key + " ");
-    display(root.left);
-}
-// Driver code
-public static void main(String args[])
-{
-    LeftSkewed obj = new LeftSkewed();
-	Node root = newNode(1);
-	root.left = newNode(2);
-	root.left.left = newNode(3);
+    static Node newNode(int key)
+    {
+        Node temp = new Node();
+        temp.key = key;
+        temp.left = temp.right = null; // Initialize children as null
+        return temp;
+    }
 
-    System.out.print("Left Skewed Binary Tree : ");
-    obj.display(root);
-}
+    public void display(Node root)
+    {
+        if (root == null)
+        {
+            return; // Base case: If tree is empty, do nothing
+        }
+        System.out.print(root.key + " "); // Print the key of the current node
+        display(root.left); // Recursive call to display the left subtree
+    }
+    public static void main(String args[])
+    {
+        LeftSkewed obj = new LeftSkewed();
+
+        // Create a left-skewed binary tree
+        Node root = newNode(1);
+        root.left = newNode(2);
+        root.left.left = newNode(3);
+
+        System.out.print("Left Skewed Binary Tree: ");
+        obj.display(root);
+    }
 }
