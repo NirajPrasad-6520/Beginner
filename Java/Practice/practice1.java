7ff78b8bdffae8d315c543c97ea8bb383c55f3ac
Niraj <nirajprasad4y18@gmail.com>
2024-12-20 13:29:51 +0530

    HTML WEB PAGE

--- Java/Practice/practice1.java
+++ Java/Practice/practice1.java
@@ -1,20 +1,34 @@
 package Practice;
 import java.util.Arrays;
+class Node{
+    int data;
+    Node left,right;
+    public Node(int item)
+    {
+        data = item;
+        left= right=null;
+    }
+}
 public class practice1
 {
-    public static void pnumber(int n)
-    {
-        if(n == 6)
+    public static int height(Node root){
+        if(root == null)
         {
-            return;
+            return 0;
         }
-        System.out.println(n);
-
-        pnumber(n + 1);
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
     }
-    public static void main(String[] args)
-    {
-       int n = 0;
-       pnumber(n);
-}
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.right.right = new Node(5);
+        root.left.left.left = new Node(6);
+
+        int x = height(root);
+        System.out.println(x);
+    }
 }
